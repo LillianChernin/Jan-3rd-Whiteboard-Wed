6eaaae4b3a161cc195dd3599cf2f35d8dd755c78
Lillian Chernin <l.smith1654@gmail.com>
2018-01-03 09:54:06 -0600

    adds findDuplicatedNumber function

--- main.js
+++ main.js
@@ -1,11 +1,20 @@
 // Create a list of n+1 numbers
 const initList = (n) => {
   let result = [];
-  for (let i = 0; i < n; i++) {
+  for (let i = 1; i <= n; i++) {
     result.push(i);
   }
   let randomNumber = Math.round(Math.random()*n);
   result.push(randomNumber);
   result.sort((a, b) => {return a - b});
   return result;
+}
+
+
+const findDuplicatedNumber = (array) => {
+  for (let i = 0; i < array.length; i++) {
+    if ((i + 1) !== array[i]) {
+      return array[i];
+    }
+  }
 }
